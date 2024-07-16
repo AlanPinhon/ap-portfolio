ac9ad9d33cdda5309acfb9a9a8cd7f643de02f2b
Alan <pinhoneibor32@gmail.com>
2024-07-16 17:25:21 -0600

    style: fonts are updated to cheddar sans and outfit

--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -1,5 +1,5 @@
 import './ContactButtonStyles.css';
 
 export const ContactButton = () => (
-  <a className="contact-btn" href="mailto:alanpinon32@gmail.com"> Contacto </a>
+  <a className="contact-btn" href="mailto:alanpinon32@gmail.com"> Contáctame </a>
 )
