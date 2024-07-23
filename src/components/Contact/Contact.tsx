282abd3d17f32e7550c8d2444ad971f3856239be
Alan <pinhoneibor32@gmail.com>
2024-07-23 13:16:12 -0600

    refactor: updated contact section structure

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,23 +1,16 @@
-import { useContext } from 'react';
-import LightLogo from '../../assets/img/ap-logos/logo-ap-1.svg';
-import DarkLogo from '../../assets/img/ap-logos/logo-ap-2.svg';
 import { ContactButton } from '../ContactButton/ContactButton';
-import { ThemeContext } from '../../context/ThemeContext';
+import ContactPhoto from '../../assets/img/ap-photos/foto_ap_perfil.webp'
 import './ContactStyles.css';
 
 export const Contact = () => {
-
-  const {theme} = useContext(ThemeContext);
 
   return (
     <section id="contact" className="contact-container container">
       <div className="contact-info">
 
-        <div className="logo-container">
-          <img src={(theme === 'light' ? DarkLogo : LightLogo)} alt="ap_logo" className="nav-ap-logo" />
-        </div>
+        <img className='contact-img' src={ContactPhoto} alt="Alan Piñón Profile" />
 
-        <div className="contact-me-text">
+        <div className="contact-container">
           <h3 className="contact-me">¡Colaboremos juntos!</h3>
           <p className="contact-me-info">
             Estoy disponible para discutir oportunidades
@@ -26,7 +19,7 @@ export const Contact = () => {
           </p>
         </div> 
 
-        <ContactButton/>
+        <ContactButton className="contact-btn" href="mailto:alanpinon32@gmail.com">Contáctame</ContactButton>
         
       </div>
 
