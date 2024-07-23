1928727db69201c3bd63e2f82db36f979b7296d8
Alan <pinhoneibor32@gmail.com>
2024-07-22 18:00:09 -0600

    chore: Added remaining icons to project

---
+++ src/assets/icons/WebpackIcon.tsx
@@ -0,0 +1,18 @@
+export const WebpackIcon = () => {
+  return (
+    <svg className="tag-icon" width="45px" height="50px" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 45 50">
+      <g>
+        <g id="Capa_1">
+          <g id="build-tools_x2F_webpack">
+            <g id="container">
+              <g id="e0b5805d423a4ec9473ee315250968b2">
+                <path id="Shape" d="M43.6,38.5l-20.2,11.5v-8.9l12.6-6.9,7.6,4.4ZM45,37.3V13.3l-7.4,4.3v15.4l7.4,4.3ZM1.4,38.5l20.2,11.5v-8.9l-12.6-6.9-7.6,4.4ZM0,37.3V13.3l7.4,4.3v15.4l-7.4,4.3ZM.9,11.8L21.6,0v8.6l-13.3,7.3h-.1c0,0-7.3-4.2-7.3-4.2ZM44.1,11.8L23.4,0v8.6l13.3,7.3h.1c0,0,7.3-4.2,7.3-4.2Z"/>
+                <path id="Shape-2" data-name="Shape" d="M21.6,39l-12.4-6.9v-13.6l12.4,7.2v13.2ZM23.4,39l12.4-6.8v-13.6l-12.4,7.2v13.2ZM10,17l12.5-6.9,12.5,6.9-12.5,7.2-12.5-7.2Z"/>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
