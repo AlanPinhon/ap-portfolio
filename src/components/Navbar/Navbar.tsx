547d85df145262a714257a5b8a2eca875ce29c3f
Alan <pinhoneibor32@gmail.com>
2024-11-30 23:20:10 -0600

    chore: Adding menu for changing the color theme

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import LightLogo from '../../assets/img/ap-logos/black-logo-ap-horizontal.svg';
 import DarkLogo from '../../assets/img/ap-logos/white-logo-ap-horizontal.svg';
 import { ThemeContext } from '../../context/ThemeContext';
@@ -8,58 +8,56 @@ import './NavbarStyles.css'
 export const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
-  const {theme, setTheme} = useContext(ThemeContext);
-  document.querySelector('body')?.setAttribute('data-theme', theme);
+  const {theme, setTheme, setThemeMode} = useContext(ThemeContext);
   
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    document.querySelector('body')?.toggleAttribute('data-theme');
-    localStorage.setItem('theme', newTheme);
+  const toggleTheme = (selectedTheme: string) => {
+
+    if(selectedTheme === 'system') {
+      setThemeMode('system')
+      localStorage.setItem('themeMode', 'system');
+
+      const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const systemTheme = prefersDarkTheme ? 'dark' : 'light';
+
+      setTheme(systemTheme);
+      localStorage.setItem('theme', systemTheme);
+    } else {
+      setTheme(selectedTheme);
+      setThemeMode('manual');
+      localStorage.setItem('theme', selectedTheme);
+      localStorage.setItem('themeMode', 'manual');
+    }
   };
   
   const toggleMenu = () => setIsOpen(!isOpen);
   const closeMenu = () => setIsOpen(false);
-  
-  useEffect(() => {
-    const query = window.matchMedia('(prefers-color-scheme: dark)');
-
-    const handleChange = () => {
-      const newTheme = query.matches ? 'dark' : 'light';
-      setTheme(newTheme);
-      document.querySelector('body')?.setAttribute('data-theme', theme);
-      localStorage.setItem('theme', newTheme);
-    };
-
-    query.addEventListener('change', handleChange);
-
-    return () => {
-      query.removeEventListener('change', handleChange);
-    };
-  }, [theme, setTheme])
 
   return (
     <section id="navbar" className="nav-container container">
 
-    <img className="nav-ap-logo" src={(theme === 'light') ? LightLogo : DarkLogo }  alt="ap-logo"/>
-    <button onClick={toggleMenu} className="menu-btn">
-      <Icon name={isOpen ? 'CloseIcon' : 'HamburgerIcon'} className={isOpen ? 'rotate' : ''}/>
-    </button>
-    
-    <nav className={`header-nav ${isOpen ? 'show' : ''}`}>
-      <a onClick={closeMenu} className="header-nav-link" href="#about-me">Sobre mi</a>
-      <a onClick={closeMenu} className="header-nav-link" href="#experience">Experiencia</a>
-      <a onClick={closeMenu} className="header-nav-link" href="#projects">Proyectos</a>
-      <a onClick={closeMenu} className="header-nav-link" href="#contact"> Contacto </a>
-      
-      <div className="separator"></div>
-      
-      <button onClick={toggleTheme} className="theme-btn">
-        <Icon name={(theme === 'light') ? 'DarkIcon' : 'LightIcon'}/>
+      <img className="nav-ap-logo" src={(theme === 'light') ? LightLogo : DarkLogo }  alt="ap-logo"/>
+      <button onClick={toggleMenu} className="menu-btn">
+        <Icon name={isOpen ? 'CloseIcon' : 'HamburgerIcon'} className={isOpen ? 'rotate' : ''}/>
       </button>
-    </nav>
 
-    
-  </section>
+      <nav className={`header-nav ${isOpen ? 'show' : ''}`}>
+        <a onClick={closeMenu} className="header-nav-link" href="#about-me">Sobre mi</a>
+        <a onClick={closeMenu} className="header-nav-link" href="#experience">Experiencia</a>
+        <a onClick={closeMenu} className="header-nav-link" href="#projects">Proyectos</a>
+        <a onClick={closeMenu} className="header-nav-link" href="#contact"> Contacto </a>
+
+        <div className="separator"></div>
+
+        <button className="theme-btn">
+          <Icon name={(theme === 'light') ? 'DarkIcon' : 'LightIcon'}/>
+        </button>
+        <div className="theme-menu">
+          <button onClick={() => toggleTheme('light')} className="theme-option">Light</button>
+          <button onClick={() => toggleTheme('dark')} className="theme-option">Dark</button>
+          <button onClick={() => toggleTheme('system')} className="theme-option">Sistema</button>
+        </div>
+      </nav>
+
+    </section>
   )
 }
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeContext } from './ThemeContext';
 
 interface ProviderProps {
@@ -7,17 +7,36 @@ interface ProviderProps {
 
 export const ThemeProvider:React.FC<ProviderProps> = ({children}) => {
 
-  const prefersDarkThemeMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const preferDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-  const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || (prefersDarkThemeMode ? 'dark' : 'light'));
+  const initialTheme = localStorage.getItem('themeMode') === 'system'
+      ? (preferDarkTheme.matches ? 'dark' : 'light')
+      : localStorage.getItem('theme') || (preferDarkTheme.matches ? 'dark' : 'light');
 
-  const contextValue = {theme, setTheme};
+  const [theme, setTheme] = useState<string>(initialTheme);
+  const [themeMode, setThemeMode] = useState(localStorage.getItem('themeMode') || 'system');
+
+  useEffect(() => {
+    if(themeMode === 'system') {
+
+      const handleChange = () => {
+        const newTheme = preferDarkTheme.matches ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+      };
+      preferDarkTheme.addEventListener('change', handleChange);
+    
+      return () => preferDarkTheme.removeEventListener('change', handleChange);
+    }
+  }, [themeMode]);
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
-
-    <ThemeContext.Provider value={contextValue}>
+    <ThemeContext.Provider value={{theme, setTheme, themeMode, setThemeMode}}>
       {children}
     </ThemeContext.Provider>
-
   )
 }
